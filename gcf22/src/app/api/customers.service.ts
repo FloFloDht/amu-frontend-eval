0a7e68531e96989350a51477b3161e03ac921b21
FloFloDht <flo.11.italia@gmail.com>
2022-01-29 18:18:50 +0100

    commentaire sur services

--- gcf22/src/app/api/customers.service.ts
+++ gcf22/src/app/api/customers.service.ts
@@ -3,13 +3,17 @@ import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { Customers } from "../types/customers";
 
+// Factorisation des 2 constantes URLs et clé d'API afin de ne pas avoir à les répéter pour chaque appel de l'API
 const SUPABASE_URL = 'https://eljkrkenpctuocyweeao.supabase.co/rest/v1/customers';
 const SUPABASE_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNjQyMTY5ODg4LCJleHAiOjE5NTc3NDU4ODh9.quGm6-7iNzla7COqnN272nD0BAJJeeEghANG_0v1YgQ";
 @Injectable()
 export class CustomersService{
 
     constructor(private http: HttpClient){ }
-
+ 
+    /**
+     * Récupère l'ensemble des lignes de l'API et retourne un tableau de clients
+     */
     findAllCustomers() : Observable<Customers>{
         return this.http.get<Customers>(SUPABASE_URL, {
             headers: {
@@ -19,6 +23,9 @@ export class CustomersService{
         });
     }
 
+    /**
+     * Création d'un nouveau client auprès de l'API qui nous le retournera ensuite
+     */
     create(fullName: string, email: string): Observable<Customers>{
         return this.http.post<Customers>(SUPABASE_URL, {
             fullName: fullName,
@@ -32,6 +39,9 @@ export class CustomersService{
         });
     } 
 
+    /**
+     * Récupère un client auprès de l'API grâce à son identifiant 
+     */
     findOne(id: number): Observable<Customers>{
         return this.http.get<Customers>(SUPABASE_URL + '?id=eq.' + id, {
             headers: {
--- gcf22/src/app/api/invoices.service.ts
+++ gcf22/src/app/api/invoices.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { Invoices, Invoice } from "../types/invoices";
 
+// Factorisation des 2 constantes URLs et clé d'API afin de ne pas avoir à les répéter pour chaque appel de l'API
 const SUPABASE_URL = 'https://eljkrkenpctuocyweeao.supabase.co/rest/v1/invoices';
 const SUPABASE_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNjQyMTY5ODg4LCJleHAiOjE5NTc3NDU4ODh9.quGm6-7iNzla7COqnN272nD0BAJJeeEghANG_0v1YgQ";  
 
@@ -11,6 +12,10 @@ export class InvoicesService {
 
     constructor(private http: HttpClient){ }
 
+    /**
+     * 
+     * Récupère l'ensemble des lignes de l'API et fonction de l'id du client et retourne un tableau des factures pour chacun des clients
+     */
     findAllByCustomerId(idCustomer:number):Observable<Invoices> {
         return this.http.get<Invoices>(SUPABASE_URL + '?idCustomer=eq.' + idCustomer, {
           headers: {
@@ -21,6 +26,10 @@ export class InvoicesService {
         });
     } 
 
+    /**
+     * 
+     * Création d'une nouvelle facture pour un utilisateur donnée auprès de l'API qui nous la retournera ensuite 
+     */
     create(amount: number, status: string, idCustomer: number): Observable<Invoice>{
         return this.http.post<Invoice>(SUPABASE_URL, {
             amount: amount,
@@ -34,9 +43,6 @@ export class InvoicesService {
             }
         });
     } 
-
-
-
 
 
 }
